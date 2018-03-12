a471a5991a3415398a43a629a911154b0af6ec4e
Ryan Gonzalez <rymg19@gmail.com>
2018-03-12 10:45:23 -0500

    0.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as fp:
 setup(
     name='mrkd',
     description='Write man pages using Markdown, and convert them to Roff or HTML',
-    version='0.1.2',
+    version='0.1.3',
     author='Ryan Gonzalez',
     author_email='rymg19@gmail.com',
     license='BSD',
