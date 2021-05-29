8171a8b78a371d08b75105f005b8aa0b92784562
Ryan Gonzalez <rymg19@gmail.com>
2021-05-28 19:35:49 -0500

    Update packaging files

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as fp:
-    requirements = fp.read().splitlines()
+with open('README.md') as fp:
+    readme = fp.read()
 
 setup(
     name='mrkd',
-    description='Write man pages using Markdown, and convert them to Roff or HTML',
     version='0.1.7',
+    description='Write man pages using Markdown, and convert them to Roff or HTML',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='Ryan Gonzalez',
     author_email='rymg19@gmail.com',
     license='BSD',
     url='https://github.com/refi64/mrkd',
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['mrkd=mrkd:main', ],
+        'console_scripts': ['mrkd=mrkd:main'],
     },
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=['Jinja2', 'mistune', 'pygments'],
     classifiers=[
         'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: BSD License',
